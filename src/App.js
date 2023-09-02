5a24f4557e4eb74f95178a5c31570827c5074da2
Aryan D <aryan.d.dalal@gmail.com>
2023-09-02 15:17:57 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -5,22 +5,29 @@ import QuestionVectors from './Weights.js'
 
 function App() {
   return (
-    <div className='page-container'>
-      <h2>
-        Personality Survey of MAHE (2023)
-      </h2>
-      <h3>
-        About:
-      </h3>
-      <p style={{fontSize:"medium"}}>
-        This is a personality survey conducted by me, spicybeanos(on github).<br/>
-        This survey is based on the 16 personality quiz. <br/>
-        I wanted to know the general populace' mbti type and how many of each are there <br/>
-        This project is just for fun :)
-        <br/>
-      </p>
-      <br />
-      <ReanderQuestions />
+    <div>
+      <div className='page-container'>
+        <h2>
+          Personality Survey of MAHE (2023)
+        </h2>
+
+        <div className='info'>
+          <h3>
+            About:
+          </h3>
+          <div>
+            This is a personality survey conducted by me, spicybeanos(on github).<br />
+            This survey is based on the 16 personality quiz. <br />
+            I wanted to find out the general populace' mbti type and how many of each are there <br />
+            This project is just for fun :)
+            <br />
+          </div>
+        </div>
+
+        <br />
+        <ReanderQuestions />
+      </div>
+
     </div>
   );
 }
